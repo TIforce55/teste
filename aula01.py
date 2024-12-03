2e067ccbe5a81e63b8eebaa2c09f9e754a6692f4
TIforce55 <dgaiosouza16@gmail.com>
2024-12-02 20:52:40 -0400

    v1.0

--- aula01.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app_Daniel = Flask (__name__)
-
-@app_Daniel.route('/')
-def raiz():
-    return 'Olá, turma!'
-
-app_Daniel.run()
--- importando.py
+++
@@ -1,8 +0,0 @@
-from aula02 import app_Daniel, saudacoes
-
-print("O objeto Flask foi importado com sucesso")
-
-print( saudacoes('Daniel'))
-
-if __name__ == "__main__":
-    app_Daniel.run()
