073f8c7ed84270f4a04412935e7146e74895ac7a
TIforce55 <dgaiosouza16@gmail.com>
2024-12-02 20:59:57 -0400

    v1.2

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 from flask import Flask, render_template
+app_Daniel=Flask(__name__)
+@app_Daniel.route('/<id>')
+def saudacao(id):
+    return render_template('homepage_nome.html', nome=id)
 
-app_Daniel = Flask(__name__ , template_folder='template')
+@app_Daniel.route("/usuario/<nome_usuario>;<nome_profissao>")
 
-@app_Daniel.route("/index")
-def indice():
-    return render_template ("index.html")
+def usuario (nome_usuario, nome_profissao):
 
-@app_Daniel.route("/")
-def homepage():
-    return render_template ("homepage.html")
-
-@app_Daniel.route("/contato")
-def contato():
-    return render_template("contato.html")
-
-@app_Daniel.route("/usuario")
-def dados_usuario():
-    nome_usuario="Daniel"
-    return render_template("usuario.html", nome = nome_usuario )
+    return render_template ("usuario.html", nome=nome_usuario)
 
 
-if __name__ == "__main__":
-    app_Daniel.run(port = 8000)
